45d1c44d3f4e20c2927631cc0d3fcda75d0eef43
jbozarov <tech@jbozarov.com>
2021-05-26 15:07:05 -0400

    now envdetails endpoint getting all env details

--- src/main/java/io/javabrains/springbootconfig/GreetingController.java
+++ src/main/java/io/javabrains/springbootconfig/GreetingController.java
@@ -2,6 +2,7 @@ package io.javabrains.springbootconfig;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -33,11 +34,20 @@ public class GreetingController {
     @Autowired
     private DbSettings dbSettings;
 
+    @Autowired
+    private Environment environment;
+
     @GetMapping("/greeting")
     public String greeting() {
         System.out.println(appDescription);
         System.out.println(listOfValues);
         System.out.println();
         return dbSettings.getConnection() + " " + dbSettings.getHost() + " " + dbSettings.getPort();
+    }
+
+
+    @GetMapping("/envdetails")
+    public String getEnvDetails() {
+        return environment.toString();
     }
 }
