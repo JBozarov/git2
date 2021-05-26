2c346f7e80328f8751b4805700b0bc71b375aac7
jbozarov <tech@jbozarov.com>
2021-05-26 12:57:41 -0400

    List of Values in the property file

--- src/main/java/io/javabrains/springbootconfig/GreetingController.java
+++ src/main/java/io/javabrains/springbootconfig/GreetingController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class GreetingController {
 
@@ -19,9 +21,13 @@ public class GreetingController {
     @Value("${my.notExistingProp: default: Some Default Value}")
     private String defaultValue;
 
+    @Value("${my.list.values}")
+    private List<String> listOfValues;
+
     @GetMapping("/greeting")
     public String greeting() {
         System.out.println(appDescription);
+        System.out.println(listOfValues);
         return greetingMessage +" " + staticValue +" "+ defaultValue;
     }
 }
